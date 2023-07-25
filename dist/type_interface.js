1adea8d330dbde26179426488698162fcd022e1a
João Souza <joao@consultorweb.cnt.br>
2023-07-25 07:22:53 -0300

    removing folders

--- dist/type_interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Employee = {
-    Name: 'Aristóteles',
-    Age: 25,
-    Occupation: 'Desenvolvedor(a) Pleno'
-};
-console.log(Employee);
-//# sourceMappingURL=type_interface.js.map
